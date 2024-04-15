f6c119eecc1bb9513594e2db2dbbc16dc6901ad8
Sebas Borjas W3 <sborjas@gaon.mx>
2024-04-15 15:05:19 -0600

    Resolved merge conflicts in operations.py

--- operations.py
+++ operations.py
@@ -2,7 +2,8 @@ a = int(input(“Give me the first number:”))
 b = int(input(“Give me the second number:”))
 
 # Display the addition
-
+print(a+b)
 # Display the subtraction
 
-# Display multiplication
+
+
